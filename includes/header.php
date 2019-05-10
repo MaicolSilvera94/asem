0d2da106c11adcbfa991bb56c3720805611f1134
Maicol Esteban Silvera Ayala <m_silvera@cde.csj.gov.py>
2019-05-10 12:44:58 -0400

    version1

---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<header>
+		<nav class="navbar navbar-default navbar-fixed-top">
+  			<div class="container">
+			    <!-- Brand and toggle get grouped for better mobile display -->
+			    <div class="navbar-header">
+				        <span class="sr-only">Toggle navigation</span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+	      				<a class="navbar-brand" href="#">
+	      					<h4>ASEM INFORMATICA</h4>
+						</a>
+			      		<a class="navbar-brand" href="#">
+			      			<img src="images/logoasem.jpg" width="60px">
+			      		</a>
+	    		</div>
+				<ul class="nav navbar-nav navbar-right">
+			      	<a class="navbar-brand" href="https://www.facebook.com/">
+	      				<img src="images/facebookicon.ico" width="40px">
+	      			</a>
+	      			<a class="navbar-brand" href="#">
+	      				<img src="images/Instagramicon.ico" width="40px">
+	      			</a>
+	      			<a class="navbar-brand" href="#">
+	      				<img src="images/whatsappicon.ico" width="40px">
+	      			</a>
+	      			<a class="navbar-brand" href="#">
+	      				<img src="images/mailicon.ico" width="40px">
+	      			</a>
+			    </ul>
+			    
+		  	</div>
+		</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>ASEM INFORMATICA</title>
+	<?php include 'includes/head.php' ?>
+	<style type="text/css">
+    </style>
+</head>
+<body style="background-color:  #ffffff;">
+	<?php include 'includes/header.php' ?>
+	<main>
+		<section class="container-sli">
+			<div id="slides" class="owl-carousel owl-theme">
+				<div class="item">
+					<img src="images/sliders/1.jpg">
+				</div>
+				<div class="item">
+				  	<img src="images/sliders/3.jpg">
+				</div>
+			</div>
+		</section>
+
+		<section class="section-info">
+			<div class="container">
+				<h2 class="title">Reparacion y Mantenimietos de Equipos Informaticos</h2>
+				<div class="row">
+					<div class="col-sm-4">
+
+							<div class="box-info">
+								<img class="imgdiv" src="images/pc.ico" width="70px">
+								<h3>Computadoras y Laptops:</h3>
+								<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+
+					</div>
+					<div class="col-sm-4">
+
+						<div class="box-info">
+							<img class="imgdiv" src="images/impresora.ico" width="70px">
+							<h3>Impresoras:</h3>
+							<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							</p>
+
+						</div>
+					</div>
+					<div class="col-sm-4">
+
+							<div class="box-info">
+								<img class="imgdiv" src="images/ventas.ico" width="70px">
+								<h3>Ventas:</h3>
+								<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="section-info">
+			<div class="container">
+				<h2 class="title">Instalación de Cámaras de Seguridad</h2>
+				<div class="row">
+					<div class="col-sm-4">
+
+							<div class="box-info">
+								<img class="imgdiv" src="images/pc.ico" width="70px">
+								<h3>Computadoras y Laptops:</h3>
+								<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+
+					</div>
+					<div class="col-sm-4">
+
+						<div class="box-info">
+							<img class="imgdiv" src="images/impresora.ico" width="70px">
+							<h3>Impresoras:</h3>
+							<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							</p>
+
+						</div>
+					</div>
+					<div class="col-sm-4">
+
+							<div class="box-info">
+								<img class="imgdiv" src="images/ventas.ico" width="70px">
+								<h3>Ventas:</h3>
+								<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="section-info">
+			<div class="container">
+				<h2 class="title">Desarrollo de Paginas Web</h2>
+				<div class="row">
+					<div class="col-sm-4">
+
+							<div class="box-info">
+								<img class="imgdiv" src="images/pc.ico" width="70px">
+								<h3>Computadoras y Laptops:</h3>
+								<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+
+					</div>
+					<div class="col-sm-4">
+
+						<div class="box-info">
+							<img class="imgdiv" src="images/impresora.ico" width="70px">
+							<h3>Impresoras:</h3>
+							<p>
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							</p>
+
+						</div>
+					</div>
+					<div class="col-sm-4">
+
+							<div class="box-info">
+								<img class="imgdiv" src="images/ventas.ico" width="70px">
+								<h3>Ventas:</h3>
+								<p>
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+								</p>
+							</div>
+
+					</div>
+				</div>
+			</div>
+		</section>
+
+	</main>
+	<?php include 'includes/script.php'; ?>
+</body>
+</html>
